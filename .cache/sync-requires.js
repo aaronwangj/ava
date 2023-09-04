d10a632c9e223ad48c4f96ece653a6ea258f9dae
Aaron <56976471+aaronwangj@users.noreply.github.com>
2023-09-04 10:34:11 -0400

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/404.js")),
+  "component---src-pages-archive-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/archive.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/pensieve/index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/pensieve/tags.js")),
+  "component---src-templates-post-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/templates/post.js")),
+  "component---src-templates-tag-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/templates/tag.js"))
+}
+
