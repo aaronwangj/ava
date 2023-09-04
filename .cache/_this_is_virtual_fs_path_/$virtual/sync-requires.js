7baa764493ed15fc9f4b4f54d36f762c0f2cef84
Aaron <56976471+aaronwangj@users.noreply.github.com>
2023-09-04 12:18:21 -0400

    second push

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/.cache/dev-404-page.js")),
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/.cache/caches/gatsby-plugin-offline/app-shell.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/404.js")),
   "component---src-pages-archive-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/archive.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/aaron/Desktop/Github/ava/src/pages/index.js")),
